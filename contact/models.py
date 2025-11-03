6ebd45b2b0297f8aa706f7075a714b254378610a
Caiua <caiua777@gmail.com>
2025-11-03 15:42:29 -0300

    Primeiro modulo criado

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+# id (primary key - automático criado pelo django)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+
+# Depois
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (imagem)
+
+class Contact(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    phone=models.CharField(max_length=50)
+    email=models.EmailField(max_length=254, blank=True) #blank da opicional no cadastro 
+    created_date= models.DateTimeField(default=timezone.now)
+    descripicion= models.TextField(blank=True)
