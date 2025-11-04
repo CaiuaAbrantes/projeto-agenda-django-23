f3e62e867ccc793986e1213be9ba028d338d5bb5
Caiua <caiua777@gmail.com>
2025-11-04 09:27:50 -0300

    c=Criando um ImageField e configurando MEDIA_URL e MEDIA_ROOT no settings.py

--- contact/models.py
+++ contact/models.py
@@ -5,18 +5,20 @@ from django.utils import timezone
 # id (primary key - automático criado pelo django)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
 
-# Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+#Depois
+#owner (foreign key)
+
 
 class Contact(models.Model):
-    first_name=models.CharField(max_length=50)
-    last_name=models.CharField(max_length=50)
-    phone=models.CharField(max_length=50)
-    email=models.EmailField(max_length=254, blank=True) #blank da opicional no cadastro 
-    created_date= models.DateTimeField(default=timezone.now)
-    descripicion= models.TextField(blank=True)
-
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True) #blank da opicional no cadastro 
+    created_date = models.DateTimeField(default=timezone.now)
+    descripicion = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
