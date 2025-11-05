a89a3b8ad3f409bd9beea01a795ff70dc685b12b
Caiua <caiua777@gmail.com>
2025-11-05 18:14:21 -0300

    mudando o titulo dos site e criando partials em global

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -7,6 +7,7 @@ def index(request):
         .order_by('-id')[5:20]
     context={
         'contacts': contacts,
+        'site_title': "Contatos - "
     }
     return render(
         request,
@@ -19,9 +20,11 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
