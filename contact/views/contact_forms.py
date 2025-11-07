d6dde50696ee884229c94daa15663770470c5d0b
Caiua <caiua777@gmail.com>
2025-11-07 10:14:07 -0300

    movendo forms para forms.py

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,28 +1,5 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.db.models import Q
-from contact.models import Contact
-from django.core.paginator import Paginator
-from django import forms
-from django.core.exceptions import ValidationError
-# Create your views here.
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model= Contact
-        fields= 'first_name', 'last_name', 'phone',
-    def clean(self):
-        cleaned_data= self.cleaned_data
-        self.add_error(
-            None,
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-
-        )
-        print(cleaned_data)
-        return super().clean()
-    
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 def create(request):
     if request.method== 'POST':
